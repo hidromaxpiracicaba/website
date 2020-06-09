ad9c25bfe26f522f0a8688dfa923839689596c8c
hidromaxpiracicaba <65093963+hidromaxpiracicaba@users.noreply.github.com>
2020-06-09 12:31:01 -0500

    update contact form
    add credentials

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'https://hidromaxpiracicaba.com.br/',
+    'username' => 'hidromaxpiracicaba@gmail.com',
+    'password' => '404529HidromaxGitHub!#',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
